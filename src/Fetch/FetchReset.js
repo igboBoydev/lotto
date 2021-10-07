d1984e595c98c3f215990c0e45631790a25c9323
xtremeCode <abelkelly6022@gmail.com>
2021-10-07 15:50:14 +0100

    push

--- src/Fetch/FetchReset.js
+++ src/Fetch/FetchReset.js
@@ -1,692 +1,230 @@
-// *{
-//     margin: 0;
-//     padding: 0;
-//     box-sizing: border-box;
-//     font-size: 1em;
-// }
+// #!/usr/bin/env nodejs
+// const express = require('express');
+// const app = express();
+// const http = require('http').Server(app);
+// const io = require('socket.io')(http, {
+//     cors: {
+//         origin: "*",
+//         methods: ["GET", "POST"]
+//     }
+// });
 
-// body{
-//     font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
-//     font-weight: 200;
-//     line-height: 1;
-// }
 
-// .register_section{
-//         box-sizing: border-box;
-//         width: 80%;
-//         box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
-//         margin: 2rem auto;
-//         border-radius: .25rem;
+// const helpers = require('./config/helpers');
+// const bodyParser = require('body-parser');
+// const fs = require('fs');
+// const path = require('path');
+// const morgan = require('morgan');
+// const cors = require('cors');
+// const passport = require('passport');
+// //const jwt_decode = require('jwt-decode');
+// const helmet = require('helmet');
+// const compression = require('compression');
+// const xmlparser = require('express-xml-bodyparser');
+// const db = require('./database/db');
+
+// //import all games
+// let game = require('./seeders/202106292222-lottos')
+
+// //Load Environment Variables
+// require('dotenv').config();
+// //Connect to DataBase
+// require('./database/db');
+
+// // set security HTTP headers
+// app.use(helmet());
+
+// //sanitize request data
+// //app.use(xss());
+
+// //gzip compression
+// app.use(compression());
+
+// //Cross origin fix
+// app.use(cors());
+// //app.options('*', 'cors');
+
+// // Logger
+// // create a write stream (in append mode)
+// var accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' })
+
+// // setup the logger
+// app.use(morgan('combined', { stream: accessLogStream }));
+
+// //Parses requests body
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
+
+// //Initialise Passport
+// app.use(passport.initialize());
+// //app.use(passport.session());
+
+// //Configure socket global
+// app.use(function (req, res, next){
+//     req.io = io;
+//     next();
+// });
+
+// //Cors
+// app.use((req, res, next) => {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header(
+//         "Access-Control-Allow-Headers",
+//         "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+//     );
+
+//     if(req.method == "OPTIONS")
+//     {
+//         res.header('Access-Control-Allow-Methods', 'PUT, POST, DELETE, PATCH, GET');
+//         return res.status(200).json({});
 //     }
 
-//         .register_section:hover{
-//         box-shadow: rgb(38, 57, 77) 0px 20px 30px -10px;
-//         transition: 1s ease-in;
+//     next();
+// })
+
+// //include route module
+// const publicRoute = require('./routes/public');
+// const authRoute = require('./routes/auth');
+
+// //App Routes
+// app.use('/api/v1/', publicRoute);
+// app.use('/api/v2/auth/', authRoute);
+
+
+// let arr = []
+
+// async function getResult() {
+    
+//     setInterval( async() => {
+//         const number = Math.floor(Math.random() * 90)
+//         const number1 = Math.floor(Math.random() * 90)
+//         const number2 = Math.floor(Math.random() * 90)
+//         const number3 = Math.floor(Math.random() * 90)
+//         const number4 = Math.floor(Math.random() * 90)
+//         arr = [number, number1, number2, number3, number4]
+//         let numberSet = new Set(arr)
+//         let array = [...numberSet]
+
+//         var d = new Date();
+//         var minutes = d.getMinutes()
+//         var seconds = d.getSeconds()
+//         var hours1 = d.getHours()
+//         var date = d.getDate();
+//         var month = d.getMonth() + 1; 
+//         var year = d.getFullYear();
+
+//         var time1 = hours1 + ":" + minutes + ":" + seconds
+ 
+//         var dates = `${year}-${month}-${date} ${time1}`
+
+//         await db.Gameresults.create({
+//             name: 'Lotto Express',
+//             odds: array.toString(),
+//             dates: dates
+//         });
+
+//     }, 1800000)
+
+// }
+    
+// getResult()
+
+
+// let setArr = []
+
+// async function getSoftLottoOdds() {
+    
+//     setInterval( async() => {
+//         const number = Math.floor(Math.random() * 90)
+//         const number1 = Math.floor(Math.random() * 90)
+//         const number2 = Math.floor(Math.random() * 90)
+//         const number3 = Math.floor(Math.random() * 90)
+//         const number4 = Math.floor(Math.random() * 90)
+//         setArr = [number, number1, number2, number3, number4]
+//         let numberSet = new Set(setArr)
+//         let array = [...numberSet]
+
+//         var d = new Date();
+//         var minutes = d.getMinutes()
+//         var seconds = d.getSeconds()
+//         var hours1 = d.getHours()
+//         var date = d.getDate();
+//         var month = d.getMonth() + 1; 
+//         var year = d.getFullYear();
+
+//         var time1 = hours1 + ":" + minutes + ":" + seconds
+ 
+//         var dates = `${year}-${month}-${date} ${time1}`
+
+//         await db.Gameresults.create({
+//             name: 'soft Lotto',
+//             odds: array.toString(),
+//             dates: dates
+//         });
+
+//     }, 300000)
+
+// }
+    
+// getSoftLottoOdds()
+
+         
+
+// //Error handling
+// app.use( (err, req, res, next) => {
+//     const error = new Error(err.message);
+//     error.status = 401;
+//     next(error);
+// });
+
+// app.use((error, req, res, next) => {
+//     if(error.message == "Unauthorized from server")
+//     {
+//         return res.status(401).json(
+//             helpers.sendError("Email does not exist")
+//         );
 //     }
+    
+//     res.status(error.status || 500);
+//     res.json({
+//         error: {
+//             status: "ERROR",
+//             message: error.message
+//         }
+//     })
+// })
 
-// @media(min-width: 840px) {
-//     .register_section{
-//         box-sizing: border-box;
-//         width: inherit;
-//         box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
-//         margin: 2rem 10rem 5rem 10rem;
-//         padding: 1rem;
-//         border-radius: .25rem;
+// // Error Handling middleware
+// app.use((err, req, res, next) => {
+//     let errCode, errMessage
+  
+//     if (err.errors) {
+//       // mongoose validation error
+//       errCode = 400 // bad request
+//       const keys = Object.keys(err.errors)
+//       // report the first validation error
+//       errMessage = err.errors[keys[0]].message
+//     } else {
+//       // generic or custom error
+//       errCode = err.status || 500
+//       errMessage = err.message || 'Internal Server Error'
 //     }
+//     res.status(errCode).type('txt')
+//       .send(errMessage)
+// })
 
-//     .register_section:hover{
-//         box-shadow: rgb(38, 57, 77) 0px 20px 30px -10px;
-//         transition: 1s ease-in;
+// //Landing Page
+// app.use('/', function(req, res, next){
+//     res.status(200).json({ suceess: true });
+//     console.log(game.up.arguments)
+// });
+
+// const PORT = process.env.PORT || 3014;
+
+// http.listen(PORT, err => {
+//     if (err) {
+//         throw err;
+//     } else {
+//         console.log('Server running on port: '+PORT);
 //     }
-// }
-
-// .scrollbar{
-//     padding-left: 10px;
-//     overflow-x: hidden;
-//     overflow-y: scroll;
-// }
-
-// @media(min-width: 1000px){
-//     .scroller{
-//     height: 100vh;
-// }
-
-// .scroll_game{
-//     overflow-x: hidden;
-//     overflow-y: scroll;
-// }
-
-// }
-
-// @media(max-width: 1000px){
-//     .form_input{
-//         width: 50vw;
-//         font-size: .775rem;
-//     }
-// }
-
-// .scrollContent{
-//     height: 100vh;
-//     text-align: center;
-// }
-
-// p.category_p{
-//     border-top: 1px solid rgb(184, 180, 180);
-//     border-width: 100%;
-//     color: #fff;
-//     font-weight: 300;
-//      padding-top: 5px;
-//     cursor: pointer;
-// }
-
-// /* game_check */
-
-// @media(min-width: 690px){
-//     section.game_section{
-//     margin-top: 5rem;
-//     margin-bottom: 20px;
-//     justify-content: center;
-//     align-items: center;
-// }
-
-// .register_btn{
-//     color: #28a745 !important;
-//     border: 1px solid #28a745;
-//     font-size: .875rem;
-//     border-radius: .15rem;
-//     padding: .55rem .5rem;
-//     margin-top: 5px !important;
-// }
-
-// .game_btn{
-//    display: inline;
-//    width: 40px;
-//    background: none;
-//    border-radius: 50%;
-//    padding: 3px;
-//    margin: 2px;
-//    background: rgb(82, 79, 79);
-//    flex-direction: column;
-//    color: #fff;
-//    align-items: center;
-//    justify-content: center;
-// }
-
-// .game_type_desc{
-//     margin-top: 8rem;
-//     color: #fff;
-//     font-weight: 100;
-// }
-
-// .betHistory_section{
-//     display: flex;
-//     justify-content: center;
-//     align-items: center;
-//     width: 80vw;
-//     flex-direction: row;
-//     flex-wrap: wrap;
-//     margin: .6rem;
-//     font-size: 11px;
-//     border: 1px solid grey;
-//     padding: .2rem;
-//     border-radius: .3rem;
-//     background: #28a745;
-// }
-
-// .p_bets{
-//     padding-top: .2rem;
-//     color: #fff;
-//     margin-right: .4rem;
-//     padding-right: .4rem;
-//     border-right: 1px solid #fff;
-// }
-
-// .p_bets:last-child{
-//     border: none;
-// }
-
-// .bets_span{
-//     color: yellow;
-// }
-// }
-
-
-
-// @media(max-width: 689px){
-//     section.game_section{
-//     width: 100vw;
-//     margin-top: 2rem;
-//     margin-bottom: 20px;
-//     justify-content: center;
-//     align-items: center;
-//     }
-
-//     .footer_section_md{
-//         width: 80vw;
-//     }
-
-//     .h5_small{
-//         font-size: 10px;
-//     }
-
-//     /* .betHistory_section{
-//     display: flex;
-//     justify-content: center;
-//     align-items: center;
-//     width: 60vw;
-//     flex-direction: column;
-//     flex-wrap: wrap;
-//     margin: .2rem;
-//     font-size: 10px;
-//     border: 1px solid grey;
-//     padding: .2rem;
-//     border-radius: .3rem;
-//     background: #28a745;
-// }
-
-// .p_bets{
-//     padding-top: .2rem;
-//     color: #fff;
-//     padding-bottom: .4rem;
-//     border-bottom: 1px solid #fff;
-// }
-
-// .p_bets:last-child{
-//     border: none;
-// } */
-
-// .bets_span{
-//     color: yellow;
-// }
-
-//     .game_type_desc{
-//     margin-left: 1.2rem;
-//     margin-top: 1rem;
-//     color: #fff;
-//     font-weight: 100;
-//     }
-
-//     .small_message{
-//         font-size: 11px;
-//         margin: 10px;
-//     }
-
-//     .small_class{
-//         margin-right: 10px;
-//     }
-
-//     .register_btn{
-//     color: #28a745 !important;
-//     border: 1px solid #28a745;
-//     font-size: .875rem;
-//     border-radius: .15rem;
-//     padding: .25rem .5rem;
-//     margin-top: 10px !important;
-// }
-// }
-
-// /* .small{
-//     margin-left: 20px;
-// } */
-
-// .btn_class{
-//     margin: 8px;
-//     margin-left: 1.2rem;
-// }
-
-
-// .small{
-//     margin-left: 10px;
-// }
-
-// .game_btn{
-//    display: inline;
-//    width: 30px;
-//    font-size: 15px;
-//    background: none;
-//    border-radius: 50%;
-//    margin: 2px;
-//    background: rgb(82, 79, 79);
-//    flex-direction: column;
-//    color: #fff;
-//    align-items: center;
-//    justify-content: center;
-// }
-
-
-
-
-// .game_header{
-//     color: #fff;
-//     font-size: 20px;
-//     font-weight: 100;
-//     margin-left: 2rem;
-// }
-
-
-
-
-// .game_col{
-//     background: rgb(46, 46, 46);
-// }
-
-
-
-// /* .form_width{
-//     width: 25vw
-// } */
-// .sidebar_dates{
-//     color: #fff;
-//     font-weight: 100;
-//     cursor: pointer;
-// }
-
-
-
-// .game_clicked{
-//     background: green;
-//     border-color: green;
-// }
-
-// .days_column.scrollbar > h6{
-//     text-align: center;
-// }
-
-// p.category_p:hover, p.category_p:focus{
-//     background: #146427;
-//    box-shadow: rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px;
-//    transition: 1s ease-in;
-// }
-
-// .ballNumber{
-//     display: flex;
-//     flex-wrap: wrap;
-//     padding: 5px;
-// }
-
-// @media(min-width: 998px){
-//     .img_width:first-child{
-//         margin-left: 3rem;
-//     }
-
-//     .img_width{
-//         width: 150px;
-//         height: 150px;
-//     }
-// }
-
-// @media(min-width: 695px){
-//     .section_container{
-//         margin-left: 50px !important;
-//     }
-
-//     .input_width{
-//         width: 80%;
-//     }
-// }
-
-// .nav_height{
-//     height: fit-content;
-// }
-
-
-
-// .img_width{ 
-//     width: 10em;
-// }
-
-// footer.footer{
-//     background: #1f8236;
-//     bottom: 0;
-// }
-
-// .apple_btn{
-//     background: #fff;
-//     width: max-content;
-//     margin: .8rem;
-//     border-radius: .3rem;
-//     color: #1f8236;
-// }
-
-// .color1{
-//     color: #1f8236;
-// }
-
-// .color2{
-//     color: #1f8236;
-// }
-
-// .socials_btn{
-//     width: max-content;
-//     margin: .8rem;
-//     border-radius: .3rem;
-// }
-
-// .backg{
-//     box-sizing: content-box;
-//     padding: .3rem;
-//     background: #fff;
-//     border-radius: .3rem;
-// }
-
-// .color3{
-//     color: #1877f2;
-// }
-
-// .color4{
-//     background: #1877f2;
-//     color: white;
-// }
-
-// .color5{
-//     color: #f24b18dc;
-// }
-
-// .color6{
-//     color: red;
-// }
-
-// .hr{
-//     color: #fff;
-//     background: #fff;
-//     width: 35rem;
-//     border: .2px solid #fff;
-// }
-
-// .display_width{
-//     width: 200px;
-// }
-
-// div.games-container{
-//     width: 80vw;
-// }
-
-// .mb-5{
-//     border-bottom: 2px solid #fff;
-//     padding-bottom: 50px;
-// }
-
-// section.games-card{
-//     background: #fbeb00;;
-//     border-radius: .35rem;
-//     font-size: 10px;
-// }
-
-// section.games-card > h3.name{
-//     font-size: 2em;
-// }
-
-// section.games-card > button{
-//     color: #fff;
-//     padding: .5rem;
-//     background: #238828;
-//     border-radius: .35rem;
-//     width: 100%;
-//     height: 40px;
-//     font-size: 15px;
-//     display: inline;
-// }
-
-// .lottoApi_Link{
-//     background:  #1f8236;
-//     width: 100%;
-//     font-size: 20px;
-//     padding: .32rem;
-//     border-radius: .25rem;
-//     color: #Fff;
-//     text-decoration: none;
-// }
-
-// .lottoApi_Link:focus, .lottoApi_Link:hover{
-//     color: #fff;
-//     list-style: none;
-//     text-decoration: none;
-// }
-
-// .loading {
-//   text-align: center;
-//   margin-top: 5rem;
-// }
-
-// .days_column{
-//     background: #28a745;
-//     height: max-content;
-// }
-
-// h6.draw{
-//     color: #fff;
-//     padding-top: 20px;
-// }
-
-// .days > h6{
-//     color: #fff;
-//     padding-bottom: 20px;
-//     padding-top: 20px;
-//     font-size: 12px;
-//     font-weight: 100;
-//     border-top: 1px solid #fff;
-//     cursor: pointer;
-// }
-
-// .days > h6:first-child{
-//     padding-top: 20px;
-//     border: none;
-// }
-
-// .register_game_section{
-//     background: rgb(75, 71, 71);
-//     border-radius: .45rem;
-// }
-
-// .register_game_section > h6{
-//     color: #fff;
-//     font-weight: 100;
-//     padding-bottom: 20px;
-//     padding-left: 10px;
-// }
-
-
-// .d_none{
-//     background: #28a745;
-//     height: fit-content;
-// }
-
-// .d{
-//     background: #28a745;
-//     border-radius: .35rem;
-//     height: 50px;
-// }
-
-// .game_back{
-//     margin: .2rem;
-//     font-size: 11px;
-// }
-
-// .bet_section{
-//     border: 2px solid #000;
-//     border: .25rem;
-// }
-
-// .game_slip_btn{
-//     background: rgb(240, 77, 77);
-//     box-sizing: content-box;
-//     padding: 2px;
-//     border-radius: .2rem;
-//     border-radius: #fff;
-// }
-
-// .game_slip_btn:hover, .game_slip_btn:focus{
-//     box-shadow: rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px;
-// }
-
-
-// .game_h6{
-//     color: #fff;
-//     font-weight: 200;
-//     font-size: 11px;
-// }
-
-// .p_type{
-//     color: #fff;
-//     margin-bottom: 10px;
-// }
-
-// @media(max-width: 1000px){
-//     .get_line{
-//         margin-left: 10px;
-//         padding-left: 10px;
-//     }
-// }
-
-// .cancel_game{
-//     cursor: pointer;
-//     width: 7px;
-// }
-
-// .get_line{
-//     border: 2px solid grey;
-//     border-radius: .35rem;
-//     padding: 5px;
-//     font-size: 11px !important;
-//     margin-bottom: 10px;
-//     background: rgb(163, 138, 138);
-// }
-
-// .get_line > button.games{
-//     margin-top: 1px;
-// }
-
-
-// .news{
-//     background: grey;
-// }
-// .container, .container-fluid, .container-lg, .container-md, .container-sm, .container-xl{
-//     padding-left: 0;
-//     padding-right: 0;
-// }
-
-// .row{
-//     margin-right: 0;
-// }
-// .verify{
-//     border: 1px solid #fff;
-//     width: 50vw;
-//     padding: 20px;
-// }
-
-// .btns{
-//     color: #28a745;
-//     border: 1px solid #28a745;
-//     border-radius: .15rem;
-//     padding: .35rem;
-// }
-
-// .error_message{
-//     color: hsl(0, 100%, 50%);
-// }
-
-// .btns:hover, .btns:focus{
-//     background: #28a745;
-//     color: #fff;
-//     text-decoration: none;
-// }
-
-// .validate_span{
-//     color: red;
-//     margin-bottom: .2rem;
-// }
-
-
-// .register_btn:focus, .register_btn:hover{
-//     background: #28a745;
-//     color: #fff !important;
-//     text-decoration: none;   
-// }
-
-// .form_btn > span{
-//     margin-top: 15px !important;
-// }
-
-// .form_btn > span > a {
-//     box-sizing: content-box;
-//     padding-bottom: 5px;
-// }
-
-// .register_btn#btn{
-//     margin-top: 5px;
-// }
-
-// .profile_link{
-//     margin-top: .7rem;
-//     color: rgba(0,0,0,.5) !important;
-// }
-
-
-
-// .profile_link > a.link{
-//     color: rgba(0,0,0,.5);
-// }
-
-// .profile_link > a.link:focus, .profile_link > a.link:hover{
-//     text-decoration: none;
-//     color: #28a745;
-// }
-
-
-// .btns.reset_validate_btn{
-//     margin-top: .65rem !important;
-// }
-
-// button.profile_btn{
-//     background: none;
-//     border: none;
-//     font-weight: 400;
-//     color: rgb(67, 65, 65);
-// }
-
-// a.links{
-//     text-decoration: none;
-//     color: grey;
-// }
-
-// p.profile_paragraph:first-child{
-//     padding-left: 10px;
-// }
-
-// button.profile_btn:focus{
-//     padding: .3rem;
-//     border: 10px;
-//     box-shadow: rgba(50, 50, 93, 0.25) 0px 30px 60px -12px inset, rgba(0, 0, 0, 0.3) 0px 18px 36px -18px inset;
-// }
-
-// div.paragraph_header{
-//     background: #28a745;
-//     padding-top: 10px;
-// }
-
-// p.profile_paragraph{
-//     font-size: 20px;
-//     color: #fff;
-// }
-
-// button.profile_btn:hover, button.profile_btn:focus{
-//     border-radius: .2rem;
-//     border-bottom: 2px solid grey;
-// }
-
-// .disabled{
-//     pointer-events: none;
-// }
-
-// .red{
-//     background: red;
-// }
-
-// .game_span{
-//     font-size: 10px;
-// }
-
-
-
-
-
+// });
